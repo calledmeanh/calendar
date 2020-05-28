e619b261ea0e3873b9b3f409be4365bdcb06bafa
calledmeanh <calledmeanh@gmail.com>
2020-05-29 00:44:44 +0700

    build production 29/5 12:44PM

---
+++ build/precache-manifest.4623216225433afb8574882b802fc53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdc77d456c9c63201e5ba884688a90b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aaa2a0f400a10cf3f3d",
+    "url": "/static/css/main.72b7393f.chunk.css"
+  },
+  {
+    "revision": "27607b6ac2a34942c40b",
+    "url": "/static/js/2.58d382ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58d382ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aaa2a0f400a10cf3f3d",
+    "url": "/static/js/main.c23a463d.chunk.js"
+  },
+  {
+    "revision": "74e1cbaef1fda168aa72",
+    "url": "/static/js/runtime-main.1e79158c.js"
+  }
+]);
