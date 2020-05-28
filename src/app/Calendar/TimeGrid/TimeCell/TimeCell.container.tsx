c9412ddd7188f84741c59908946aa981be57e98e
calledmeanh <calledmeanh@gmail.com>
2020-05-29 00:40:54 +0700

    update time cell

---
+++ src/app/Calendar/TimeGrid/TimeCell/TimeCell.container.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { TimeCellPresenter } from "./TimeCell.presenter";
+export declare module TimeCellContainerModule {
+  export type Props = { data: string; options: { showContent: boolean }; className?: string };
+  export type State = {};
+  export type Presenter = { data: string; options: { showContent: boolean }; className?: string };
+}
+export class TimeCellContainer extends Component<TimeCellContainerModule.Props, TimeCellContainerModule.State> {
+  render() {
+    return (
+      <TimeCellPresenter
+        data={this.props.data}
+        options={{ showContent: this.props.options.showContent }}
+        className={this.props.className}
+      />
+    );
+  }
+}
---
+++ src/app/Calendar/TimeGrid/TimeCell/TimeCell.presenter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./TimeCell.style.scss";
+import { TimeCellContainerModule } from "./TimeCell.container";
+
+export const TimeCellPresenter: React.FC<TimeCellContainerModule.Presenter> = (props) => {
+  return (
+    <div className={`time-cell ${props.className ? props.className : ""}`}>
+      {props.options.showContent && props.data}
+    </div>
+  );
+};
