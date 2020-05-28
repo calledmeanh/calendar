609902f69e0857166f496cba26392dd801bb2ac5
calledmeanh <calledmeanh@gmail.com>
2020-05-28 22:52:34 +0700

    'formatted'

--- src/app/Calendar/Header/Label/Label.presenter.tsx
+++ src/app/Calendar/Header/Label/Label.presenter.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Label.style.scss";
 import { LabelContainerModule } from "./Label.container";
 
-export const LabelPresenter: React.FC<LabelContainerModule.Presenter> = (props) => {
+export const LabelPresenter: React.FC<LabelContainerModule.Presenter> = (
+  props
+) => {
   return <div className="header-label">{props.item}</div>;
 };
--- src/app/Calendar/UserAva/UserAva.presenter.tsx
+++ src/app/Calendar/UserAva/UserAva.presenter.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./UserAva.style.scss";
 import { UserAvaContainerModule } from "./UserAva.container";
-export const UserAvaPresenter: React.FC<UserAvaContainerModule.Presenter> = (props) => {
+export const UserAvaPresenter: React.FC<UserAvaContainerModule.Presenter> = (
+  props
+) => {
   return (
     <div className="user-ava">
       <img src={props.data} alt={"user-img"} />
